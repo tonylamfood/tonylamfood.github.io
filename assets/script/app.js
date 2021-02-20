79889fc1860aa53b0bc73a5473825e8a7e45f4d7
tonylamfood <77596686+tonylamfood@users.noreply.github.com>
2021-02-21 02:36:27 +0800

    Add files via upload

---
+++ assets/script/app.js
@@ -0,0 +1,8 @@
+$("#mewe") .mouseover(function () {
+    this.src= "assets/img/logo/TheMeWeLogo-black.png";
+    $("#mewe").css("border-color","black");
+ })
+ .mouseout(function () {
+    this.src= "assets/img/logo/TheMeWeLogo-red.png";
+    $("img").css("border-color","#E51200");
+});
